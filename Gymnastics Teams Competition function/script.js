fe1fd3f4a4e08d37f4ad16fd7ba46d6f0c018586
naleilan <leilanschmitz@gmail.com>
2026-02-04 10:53:34 +0330

    fix the bug

--- Gymnastics Teams Competition function/script.js
+++ Gymnastics Teams Competition function/script.js
@@ -1,40 +1,34 @@
-// Data 1
-// let dolphinsScoreAvg = (96 + 108 + 89) / 3;
-// let koalasScoreAvg = (88 + 91 + 110) / 3;
-
-// Data Bonus 1
-// let dolphinsScoreAvg = (97 + 112 + 101) / 3;
-// let koalasScoreAvg = (109 + 95 + 123) / 3;
-
-// Data Bonus 2
-// let dolphinsScoreAvg = (97 + 112 + 101) / 3;
-// let koalasScoreAvg = (109 + 95 + 106) / 3;
-
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
-const avgDolhins = calcAverage(96, 108, 89).toFixed(1);
-const avgKoalas = calcAverage(88, 91, 110).toFixed(1);
 
-console.log(avgDolhins, avgKoalas);
+function checkWinner() {
+  // const dolphinsScoreAvg = Math.round(calcAverage(6, 18, 89));
+  // const koalasScoreAvg = Math.round(calcAverage(588, 191, 10));
 
-// checkWinner;
+  const dolphinsScoreAvg = Math.round(calcAverage(588, 191, 10));
+  const koalasScoreAvg = Math.round(calcAverage(6, 18, 89));
 
-// Logic for winner / draw / no winner
-// if (dolphinsScoreAvg > 100 && dolphinsScoreAvg > koalasScoreAvg) {
-//   console.log(
-//     `Dolphins win the trophy with an average score of ${dolphinsScoreAvg} vs Koalas ${koalasScoreAvg}`,
-//   );
-// } else if (koalasScoreAvg > 100 && koalasScoreAvg > dolphinsScoreAvg) {
-//   console.log(
-//     `Koalas win the trophy with an average score of ${koalasScoreAvg} vs Dolphins ${dolphinsScoreAvg}`,
-//   );
-// } else if (
-//   dolphinsScoreAvg >= 100 &&
-//   koalasScoreAvg >= 100 &&
-//   dolphinsScoreAvg === koalasScoreAvg
-// ) {
-//   console.log(
-//     `It's a draw! Both teams have an average score of ${dolphinsScoreAvg}`,
-//   );
-// } else {
-//   console.log("No team wins the trophy. Minimum score requirement not met.");
-// }
+  console.log(
+    dolphinsScoreAvg,
+    koalasScoreAvg,
+    dolphinsScoreAvg * 2,
+    koalasScoreAvg * 2,
+  );
+
+  if (dolphinsScoreAvg >= koalasScoreAvg * 2) {
+    console.log(
+      `Dolphins win the trophy with an average score of ${dolphinsScoreAvg} vs Koalas ${koalasScoreAvg}`,
+    );
+  } else if (koalasScoreAvg >= dolphinsScoreAvg * 2) {
+    console.log(
+      `Koalas win the trophy with an average score of ${koalasScoreAvg} vs Dolphins ${dolphinsScoreAvg}`,
+    );
+  } else if (dolphinsScoreAvg === koalasScoreAvg) {
+    console.log(
+      `It's a draw! Both teams have an average score of ${dolphinsScoreAvg}`,
+    );
+  } else {
+    console.log("No team wins the trophy. Minimum score requirement not met.");
+  }
+}
+
+checkWinner();
