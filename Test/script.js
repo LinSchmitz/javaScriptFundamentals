6e59a71f77bc6de056a174d5a0102d94151a2293
naleilan <leilanschmitz@gmail.com>
2026-02-05 11:47:34 +0330

    added test

--- Test/script.js
+++ Test/script.js
@@ -1,16 +1,30 @@
-const calcAge = function (birthYear) {
-  return 2026 - birthYear;
+//objectc
+
+const jonas = {
+  firsName: "jonas",
+  lastName: "schmedtmann",
+  job: "teacher",
+  friends: ["Michel", "peter", "stefan"],
 };
 
-const years = [1999, 1957, 2006, 2010, 1980];
+console.log(
+  `${jonas.firsName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}  `,
+);
 
-console.log(calcAge(years[0]));
-console.log(2026 - 1999);
+//
+// const calcAge = function (birthYear) {
+//   return 2026 - birthYear;
+// };
 
-const ages = [
-  calcAge(years[0]),
-  calcAge(years[1]),
-  calcAge(years[years.length - 1]),
-];
+// const years = [1999, 1957, 2006, 2010, 1980];
 
-console.log(ages);
+// console.log(calcAge(years[0]));
+// console.log(2026 - 1999);
+
+// const ages = [
+//   calcAge(years[0]),
+//   calcAge(years[1]),
+//   calcAge(years[years.length - 1]),
+// ];
+
+// console.log(ages);
