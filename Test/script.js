04c2f393e67e8048cdd717b24589d6d80b8b0d2c
naleilan <leilanschmitz@gmail.com>
2026-02-05 09:00:56 +0330

    added test

--- Test/script.js
+++ Test/script.js
@@ -6,3 +6,11 @@ const years = [1999, 1957, 2006, 2010, 1980];
 
 console.log(calcAge(years[0]));
 console.log(2026 - 1999);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+
+console.log(ages);
